0e828440a92aaaae91cb16a75b532483bf6d715f
Fernando Matos <fernando.matos221@gmail.com>
2026-07-03 18:19:18 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     static void main() {
-        
+
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
