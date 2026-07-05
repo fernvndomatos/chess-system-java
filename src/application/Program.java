c54d7b448961785fc9cd8f2353d5554ba1f915ba
Fernando Matos <fernando.matos221@gmail.com>
2026-07-05 14:45:31 -0300

    BoardException and defensive programming

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,6 @@ import chess.ChessMatch;
 
 public class Program {
     static void main() {
-
         ChessMatch chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
     }
