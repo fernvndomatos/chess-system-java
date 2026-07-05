1c0f6a5357cf9dc1b65c53d33c27dc3d8c95d398
Fernando Matos <fernando.matos221@gmail.com>
2026-07-05 15:38:28 -0300

    ChessException and ChessPosition

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -1,7 +1,7 @@
 package boardgame;
 
 public class BoardException extends RuntimeException {
-    public static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     public BoardException(String msg) {
         super(msg);
